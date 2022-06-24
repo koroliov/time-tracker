9a20d9c7e050ea440b8597034aebeef83af76d04
koroliov <d.koroliov@gmail.com>
2022-06-24 03:11:05 +0300

    Fix various error in time-tracker class

--- test/cases/06-time-tracker-time-not-restored-on-startup.test.js
+++ test/cases/06-time-tracker-time-not-restored-on-startup.test.js
@@ -2,9 +2,9 @@
 
 const tp = require('tape');
 const TestUser = require('../test-user.js');
-const initData = require('./06-time-tracker-time-not-restored-on-startup.js');
 
-tp('', async t => {
+tp('data exists', async t => {
+  const initData = require('./06-time-tracker-time-not-restored-on-startup.js');
   const u = new TestUser();
   await u.prepare();
   await u.openApp(initData);
@@ -14,6 +14,37 @@ tp('', async t => {
 
   const totalTimeOnTimeTracker = await u.timeTrackerTotalTimeGet();
   t.equal(totalTimeOnTimeTracker, '0d 0h 0m 10s');
+
+  await u.closeApp();
+  t.end();
+});
+
+tp('data exists', async t => {
+  const u = new TestUser();
+  await u.prepare();
+  await u.openApp();
+
+  t.ok(await u.noTimeEntriesPresent());
+
+  const totalTimeOnTimeTracker = await u.timeTrackerTotalTimeGet();
+  t.equal(totalTimeOnTimeTracker, '0d 0h 0m 0s');
+
+  const billableTimeOnTimeTracker = await u.timeTrackerBillableTimeGet();
+  t.equal(billableTimeOnTimeTracker, '0d 0h 0m 0s');
+
+  const targetTotal = await u.timeTrackerTargetTotalGet();
+  t.equal(targetTotal, '5d');
+
+  const targetBillablePercent = await u.timeTrackerTargetBillablePercentGet();
+  t.equal(targetBillablePercent, '50%');
+
+  const curBillablePercentOnTimeTracker =
+    await u.timeTrackerCurrentBillablePercentGet();
+  t.equal(curBillablePercentOnTimeTracker, '0%');
+
+  const billablePercentOnTimeTracker =
+    await u.timeTrackerBillbleTargetPercentGet();
+  t.equal(billablePercentOnTimeTracker, '0%');
 
   await u.closeApp();
   t.end();
--- test/test-user.js
+++ test/test-user.js
@@ -14,12 +14,14 @@ class TestUser {
   async openApp(initData) {
     await this.browser.url(`file://${
       config.pathToProjectNoTrailingSlash}/build/time-tracker.html`);
-    const str = [
-      'window.localStorage.clear();',
-      "window.localStorage.setItem('time-tracker', `",
-      initData,
-      '`);',
-    ].join(' ');
+    let str = 'window.localStorage.clear();';
+    if (initData) {
+      str += [
+        "window.localStorage.setItem('time-tracker', `",
+        initData,
+        '`);',
+      ].join(' ');
+    }
     const script = `window.addEventListener('beforeunload', function() {
       ${str};
     })`;
@@ -29,6 +31,13 @@ class TestUser {
 
   async closeApp() {
     await this.browser.deleteSession();
+  }
+
+  async noTimeEntriesPresent() {
+    const selector = `#time-tracker .children`;
+    const tt = await this.browser.$('time-tracker');
+    const e = await tt.shadow$(selector);
+    return await e.getText() === '';
   }
 
   async timeEntryTotalTimeGet(address) {
@@ -44,6 +53,43 @@ class TestUser {
 
   async timeTrackerTotalTimeGet() {
     const selector = '#time-tracker > .grid-wrapper > .total.value';
+    const tt = await this.browser.$('time-tracker');
+    const e = await tt.shadow$(selector);
+    return await e.getText();
+  }
+
+  async timeTrackerBillableTimeGet() {
+    const selector = '#time-tracker > .grid-wrapper > .billable.value';
+    const tt = await this.browser.$('time-tracker');
+    const e = await tt.shadow$(selector);
+    return await e.getText();
+  }
+
+  async timeTrackerCurrentBillablePercentGet() {
+    const selector =
+      '#time-tracker > .grid-wrapper > .billable.percent-current';
+    const tt = await this.browser.$('time-tracker');
+    const e = await tt.shadow$(selector);
+    return await e.getText();
+  }
+
+  async timeTrackerBillbleTargetPercentGet() {
+    const selector =
+      '#time-tracker > .grid-wrapper > .billable.percent-target';
+    const tt = await this.browser.$('time-tracker');
+    const e = await tt.shadow$(selector);
+    return await e.getText();
+  }
+
+  async timeTrackerTargetTotalGet() {
+    const selector = '#time-tracker > h1 .abs-value';
+    const tt = await this.browser.$('time-tracker');
+    const e = await tt.shadow$(selector);
+    return await e.getText();
+  }
+
+  async timeTrackerTargetBillablePercentGet() {
+    const selector = '#time-tracker > h1 .percent-value';
     const tt = await this.browser.$('time-tracker');
     const e = await tt.shadow$(selector);
     return await e.getText();
