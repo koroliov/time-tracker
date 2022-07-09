7e6ce4b6fbba21f8b1c46943066a95e845b7d87c
koroliov <d.koroliov@gmail.com>
2022-07-10 00:17:23 +0300

    Implement time-tracker versions

---
+++ test/manual/version-handling/version-handling.test.js
@@ -0,0 +1,49 @@
+//Open first time
+//
+//run this, then reload
+//EXPECTED: clean empty time-tracker
+(async () => {
+  window.addEventListener('beforeunload', function() {
+    this.localStorage.removeItem('/home/dk/work/github-mine/time-tracker/build/time-tracker.html');
+  });
+})();
+
+//localStorage data present, versions don't match
+//
+//add a couple of entries
+//run this, then reload
+//EXPECTED:
+// - nothing is shown, emtpy page, alert with the suggestion to use
+// - an appropriate version of time-tracker or
+// - manually delete the entry from the dev-tools
+(async () => {
+  document.querySelector('time-tracker').version = [999, 0, 0];
+})();
+
+//JSON file import, versions match
+//
+//Create a couple of entries
+//Import the import-versions-match.json file and see:
+//EXPECTED:
+// - alert with a warning about a backup etc.
+// - the entry with the title: for manual test, versions match
+// - no previously added entries
+
+//JSON file import, versions don't match
+//
+//Create a couple of entries
+//Import the import-versions-dont-match.json file and see:
+//EXPECTED:
+// - alert with a warning about a backup etc.
+// - alert with refusing to match message
+// - previously added entries unchanged
+
+//JSON file import, broken data (i.e. the file is json, but with wrong data)
+//JSON file import, garbage data (i.e. the file is something not json at all)
+//
+//Create a couple of entries
+//Import the import-versions-dont-match.json file and see:
+//EXPECTED:
+// - alert with a warning about a backup etc.
+// - alert with corrupted data
+// - previously added entries unchanged
