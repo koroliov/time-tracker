fdacba8afce1211ea4a3e24f16af8d3946239285
Dmitry Koroliov <d.koroliov@gmail.com>
2023-01-15 08:29:25 +0200

    Disable drop area for dragging over descendants
    If the user wishes to move an entry to its own children, i.e. putting it
    and its children on the same level in the tree, then he will have to
    manually move all the children as siblings of their parent (or
    ancestor, more generally). One reason behind this decision was that
    during a drag, when the dragged entry has not-collapsed children, the
    drag picture (in Chrome at least) shows the children too, which may be
    counter intuitive, since the user might expect, that the children
    (descendants, more generally) will be also moved on drop, but that's
    nonsense

--- src/handle-drop-area.js
+++ src/handle-drop-area.js
@@ -29,6 +29,13 @@ function handleDropArea(dragOverZone) {
     this.timeTracker.entryWithDropAreaCssClasses = null;
     return;
   }
+  if (isEntryOneAnscestorOfEntryTwo(this.timeTracker.entryBeingDragged,
+      this)) {
+    this.timeTracker.entryWithDropAreaCssClasses?.classList
+        .remove(...Object.values(DROP_AREA_CSS_CLASSES));
+    this.timeTracker.entryWithDropAreaCssClasses = null;
+    return;
+  }
   if (dragOverZone === 'top') {
     if (this.previousElementSibling?.classList
         .contains(DROP_AREA_CSS_CLASSES.SIBLING_BOTTOM)) {
@@ -69,6 +76,17 @@ function handleDropArea(dragOverZone) {
         .remove(DROP_AREA_CSS_CLASSES.SIBLING_BOTTOM);
     this.classList.add(DROP_AREA_CSS_CLASSES.CHILD);
     this.timeTracker.entryWithDropAreaCssClasses = this;
+  }
+
+  function isEntryOneAnscestorOfEntryTwo(entryOne, entryTwo) {
+    let ancestor = entryTwo.parentTimeEntry;
+    while (ancestor) {
+      if (ancestor === entryOne) {
+        return true;
+      }
+      ancestor = ancestor.parentTimeEntry;
+    }
+    return false;
   }
 };
 
