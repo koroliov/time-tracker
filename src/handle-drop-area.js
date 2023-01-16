8bcc6dfed8b0feeed638294dc28e664e02257d7e
Dmitry Koroliov <d.koroliov@gmail.com>
2023-01-16 18:42:26 +0200

    WIP: Partially refactor handleDropArea() module

--- src/handle-drop-area.js
+++ src/handle-drop-area.js
@@ -4,78 +4,24 @@ const DROP_AREA_CSS_CLASSES = {
   SIBLING_BOTTOM: 'drop-as-sibling-bottom',
   CHILD: 'drop-as-child',
 };
+const allDropAreaCssClassesArr = Object.values(DROP_AREA_CSS_CLASSES);
 
 function handleDropArea(dragOverZone) {
-  if (this.timeTracker.entryBeingDragged.parentTimeEntry === this) {
-    if (dragOverZone === 'top') {
-      if (this.previousElementSibling?.classList
-          .contains(DROP_AREA_CSS_CLASSES.SIBLING_BOTTOM)) {
-        return;
-      }
-      this.classList.add(DROP_AREA_CSS_CLASSES.SIBLING_TOP);
-      this.timeTracker.entryWithDropAreaCssClasses = this;
-      return;
-    }
-    if (dragOverZone === 'middle') {
-      this.classList.remove(DROP_AREA_CSS_CLASSES.SIBLING_TOP);
-      this.timeTracker.entryWithDropAreaCssClasses = null;
-      return;
-    }
-    if (dragOverZone === 'bottom') {
-      this.timeTracker.entryWithDropAreaCssClasses?.classList
-          .remove(...Object.values(DROP_AREA_CSS_CLASSES));
-      this.timeTracker.entryWithDropAreaCssClasses = null;
-      return;
-    }
-    return;
-  }
-  if (this === this.timeTracker.entryBeingDragged) {
-    this.timeTracker.entryWithDropAreaCssClasses?.classList
-        .remove(...Object.values(DROP_AREA_CSS_CLASSES));
-    this.timeTracker.entryWithDropAreaCssClasses = null;
-    return;
-  }
-  if (isEntryOneAnscestorOfEntryTwo(this.timeTracker.entryBeingDragged,
-      this)) {
-    this.timeTracker.entryWithDropAreaCssClasses?.classList
-        .remove(...Object.values(DROP_AREA_CSS_CLASSES));
-    this.timeTracker.entryWithDropAreaCssClasses = null;
-    return;
+  const timeTracker = this.timeTracker;
+  const entryOverWhichDragIsBeingPerformed = this;
+  const entryBeingDragged = timeTracker.entryBeingDragged;
+  const previousSiblingEntry = this.previousElementSibling;
+  const nextSiblingEntry = this.nextElementSibling;
+
+  if (entryIsBeingDraggedOverItsParent()) {
+    handleDragOverParent();
+  } else if (entryIsBeingDraggedOverItselfOrDescendant()) {
+    return hideAllDropAreas();
   }
   if (dragOverZone === 'top') {
-    if (this.previousElementSibling?.classList
-        .contains(DROP_AREA_CSS_CLASSES.SIBLING_BOTTOM)) {
-      return;
-    }
-    if (this.previousElementSibling === this.timeTracker.entryBeingDragged) {
-      return;
-    }
-    this.timeTracker.entryWithDropAreaCssClasses?.classList
-        .remove(...Object.values(DROP_AREA_CSS_CLASSES));
-    this.classList.add(DROP_AREA_CSS_CLASSES.SIBLING_TOP);
-    this.timeTracker.entryWithDropAreaCssClasses = this;
-    return;
-  }
-  if (dragOverZone === 'bottom') {
-    if (this.nextElementSibling?.classList
-        .contains(DROP_AREA_CSS_CLASSES.SIBLING_TOP)) {
-      return;
-    }
-    this.timeTracker.entryWithDropAreaCssClasses?.classList
-        .remove(...Object.values(DROP_AREA_CSS_CLASSES));
-    this.timeTracker.entryWithDropAreaCssClasses = null;
-    this.classList.remove(DROP_AREA_CSS_CLASSES.CHILD);
-    if (this.timeTracker.entryBeingDragged === this.nextElementSibling) {
-      this.timeTracker.entryWithDropAreaCssClasses = null;
-      return;
-    }
-    if (!this.isCollapsed && this.childEntries.length) {
-      this.timeTracker.entryWithDropAreaCssClasses = null;
-      return;
-    }
-    this.classList.add(DROP_AREA_CSS_CLASSES.SIBLING_BOTTOM);
-    this.timeTracker.entryWithDropAreaCssClasses = this;
-    return;
+    handleDragOverTop();
+  } else if (dragOverZone === 'bottom') {
+    handleDragOverBottom();
   }
   if (dragOverZone === 'middle') {
     this.classList.remove(DROP_AREA_CSS_CLASSES.SIBLING_TOP,
@@ -86,7 +32,7 @@ function handleDropArea(dragOverZone) {
       this.timeTracker.entryWithDropAreaCssClasses = null;
       return;
     }
-    this.previousElementSibling?.classList
+    previousSiblingEntry?.classList
         .remove(DROP_AREA_CSS_CLASSES.SIBLING_BOTTOM);
     if (!this.isCollapsed && this.childEntries.length) {
       this.timeTracker.entryWithDropAreaCssClasses = null;
@@ -96,17 +42,102 @@ function handleDropArea(dragOverZone) {
     this.timeTracker.entryWithDropAreaCssClasses = this;
   }
 
-  function isEntryOneAnscestorOfEntryTwo(entryOne, entryTwo) {
-    let ancestor = entryTwo.parentTimeEntry;
-    while (ancestor) {
-      if (ancestor === entryOne) {
-        return true;
-      }
-      ancestor = ancestor.parentTimeEntry;
+  function handleDragOverBottom() {
+    if (equivalentDropAreaAlreadyExists()) {
+      return;
     }
-    return false;
+    hideAllDropAreas();
+    if (noReasonToDropNextSiblingAsNextSibling()) {
+      return;
+    }
+
+    if (!entryOverWhichDragIsBeingPerformed.isCollapsed &&
+        entryOverWhichDragIsBeingPerformed.childEntries.length) {
+      return;
+    }
+    entryOverWhichDragIsBeingPerformed.classList
+      .add(DROP_AREA_CSS_CLASSES.SIBLING_BOTTOM);
+    timeTracker.entryWithDropAreaCssClasses = entryOverWhichDragIsBeingPerformed;
+
+    function noReasonToDropNextSiblingAsNextSibling() {
+      return timeTracker.entryBeingDragged === nextSiblingEntry;
+    }
+
+    function equivalentDropAreaAlreadyExists() {
+      return nextSiblingEntry?.classList
+          .contains(DROP_AREA_CSS_CLASSES.SIBLING_TOP)
+    }
   }
-};
+
+  function handleDragOverTop() {
+    if (nothingToDo()) {
+      return;
+    }
+    timeTracker.entryWithDropAreaCssClasses?.classList
+        .remove(...allDropAreaCssClassesArr);
+    entryOverWhichDragIsBeingPerformed.classList
+        .add(DROP_AREA_CSS_CLASSES.SIBLING_TOP);
+    timeTracker.entryWithDropAreaCssClasses =
+        entryOverWhichDragIsBeingPerformed;
+
+    function nothingToDo() {
+      return previousSiblingEntry === entryBeingDragged ||
+          previousSiblingEntry?.classList
+              .contains(DROP_AREA_CSS_CLASSES.SIBLING_BOTTOM)
+    }
+  }
+
+  function entryIsBeingDraggedOverItsParent() {
+    return entryBeingDragged.parentTimeEntry ===
+        entryOverWhichDragIsBeingPerformed;
+  }
+
+  function entryIsBeingDraggedOverItselfOrDescendant() {
+    return entryBeingDragged === entryOverWhichDragIsBeingPerformed ||
+        isOverDescendant();
+
+    function isOverDescendant() {
+      let ancestor = entryOverWhichDragIsBeingPerformed.parentTimeEntry;
+      while (ancestor) {
+        if (ancestor === entryBeingDragged) {
+          return true;
+        }
+        ancestor = ancestor.parentTimeEntry;
+      }
+      return false;
+    }
+  }
+
+  function handleDragOverParent() {
+    switch (dragOverZone) {
+      case 'top':
+        if (previousSiblingHasBottomDropArea(this)) {
+          return;
+        }
+        showSiblingTopDropAreaOver(entryOverWhichDragIsBeingPerformed);
+      case 'middle':
+      case 'bottom':
+        hideAllDropAreas();
+    }
+    return;
+
+    function previousSiblingHasBottomDropArea() {
+      return entryOverWhichDragIsBeingPerformed.previousElementSibling
+          ?.classList.contains(DROP_AREA_CSS_CLASSES.SIBLING_BOTTOM)
+    }
+  }
+
+  function hideAllDropAreas() {
+    timeTracker.entryWithDropAreaCssClasses?.classList
+        .remove(...allDropAreaCssClassesArr);
+    timeTracker.entryWithDropAreaCssClasses = null;
+  }
+
+  function showSiblingTopDropAreaOver(entry) {
+    entry.classList.add(DROP_AREA_CSS_CLASSES.SIBLING_TOP);
+    timeTracker.entryWithDropAreaCssClasses = entry;
+  }
+}
 
 module.exports = {
   handleDropArea,
